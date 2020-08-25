99eb42e8ac747c46fa9e990966cfd5d14e3e1f1d
Sanzensekai-mx <Ivanymn@yandex.ru>
2020-08-25 16:58:10 +0400

    End of second chapter

---
+++ Strings/EX8.py
@@ -0,0 +1,25 @@
+def str_sort(string):
+    return ''.join(sorted(string))
+
+
+# print(str_sort('bca'))
+
+
+def str_name(names):
+    return ', '.join(sorted(names.split(' ')))
+
+
+print(str_name('Tom Dick Harry'))
+
+def last_word_alpha(names):
+    return sorted(names.split(' '))[-1]
+
+
+print(last_word_alpha('Tom Dick Harry'))
+
+
+def max_len(names):
+    return sorted(names.split(' '), key=len)[-1]
+
+
+print(max_len('Tom Dick Harry'))
