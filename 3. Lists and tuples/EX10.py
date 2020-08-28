ed6406794212506cd51b8aa85a860e3239883f88
Sanzensekai-mx <Ivanymn@yandex.ru>
2020-08-28 13:03:45 +0400

    first from "beyond the exercise"

--- 3. Lists and tuples/EX10.py
+++ 3. Lists and tuples/EX10.py
@@ -7,6 +7,24 @@ def my_sum(*args):
     return output
 
 
-print(my_sum('abc', 'def'))
-print(my_sum([1, 2, 3], [4, 5, 6]))
-print(my_sum(1, 2, 3))
+# print(my_sum('abc', 'def'))
+# print(my_sum([1, 2, 3], [4, 5, 6]))
+# print(my_sum(1, 2, 3))
+# print(my_sum())
+
+
+def bigger_than(first_elem, *args):
+    if not first_elem and args:
+        return ()
+    output = args[0]
+    for item in args:
+        if item < first_elem:
+            if item == output:
+                output -= item
+            continue
+        else:
+            output += item
+    return output
+
+
+print(bigger_than(10, 5, 20, 30, 6))
