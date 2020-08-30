ed7d279bcfb99120f46561fda48d2a4dc4ca46cb
Sanzensekai-mx <Ivanymn@yandex.ru>
2020-08-30 12:54:08 +0400

    EX 13 main first optional complete

--- 3. Lists and tuples/EX13.py
+++ 3. Lists and tuples/EX13.py
@@ -18,3 +18,15 @@ PEOPLE = [('Donald', 'Trump', 7.85),
 # format_sort_records(PEOPLE)
 
 
+def format_sort_records_named_tuple(records):
+    parts_of_records = collections.namedtuple('parts_of_records', 'first last time')
+    el_records = []
+    for el in sorted(records, key=operator.itemgetter(1, 0)):
+        el_records.append(parts_of_records(first=el[0], last=el[1], time=el[2]))
+    output = []
+    for person in el_records:
+        output.append(f'{person.last:10} {person.first:10} {person.time:5.2f}')
+    print('\n'.join(output))
+
+
+format_sort_records_named_tuple(PEOPLE)
