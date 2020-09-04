30895c157292603c5c8a72e8e2dd932145913abd
Sanzensekai-mx <Ivanymn@yandex.ru>
2020-09-04 23:43:04 +0400

    EX 15 first dop complete

--- 4. Dict and set/EX15.py
+++ 4. Dict and set/EX15.py
@@ -14,6 +14,25 @@ def get_rainfall():
         print(f'{city} - {rain}')
 
 
-get_rainfall()
+# get_rainfall()
 
+
+def get_rainfall_2():
+    rainfall = {}
+    while True:
+        city_input = input('Enter city\'s name: ').strip()
+        if not city_input:
+            break
+        try:
+            mm_input = int(input('Enter mm input: '))
+        except ValueError:
+            print('You didn\'t enter a valid integer; try again.')
+            continue
+        rainfall[city_input] = rainfall.get(city_input, [])
+        rainfall[city_input].append(mm_input)
+    for city, rain in rainfall.items():
+        print(f'{city}: total - {sum(rain)}, average - {sum(rain) / len(rain)}')
+
+
+get_rainfall_2()
 
