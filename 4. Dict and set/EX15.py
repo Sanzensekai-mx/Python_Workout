377d82248f9b680c888af863a5106b2cdf792d7e
Sanzensekai-mx <Ivanymn@yandex.ru>
2020-09-08 21:23:21 +0400

    EX 15 complete without second optional

--- 4. Dict and set/EX15.py
+++ 4. Dict and set/EX15.py
@@ -1,3 +1,7 @@
+import os
+import collections
+
+
 def get_rainfall():
     rainfall = {}
     while True:
@@ -34,5 +38,18 @@ def get_rainfall_2():
         print(f'{city}: total - {sum(rain)}, average - {sum(rain) / len(rain)}')
 
 
-get_rainfall_2()
+# get_rainfall_2()
+
+def get_lens_words(file):
+    result_dict = collections.defaultdict(list)
+    with open(os.path.join(os.getcwd(), file), 'r', encoding='utf-8') as f:
+        for lines in f:
+            for word in lines[:-2].split():
+                result_dict[len(word)].append(word) if word[-1] not in '.,:;?!' else \
+                    result_dict[len(word[:-1])].append(word[:-1])
+    return result_dict
+
+
+print(get_lens_words('stix.txt'))
+
 
