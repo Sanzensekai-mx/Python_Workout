30e5e097be8a8cf3adb86b1a73c605b52c9d96d1
Sanzensekai-mx <Ivanymn@yandex.ru>
2020-09-18 23:19:12 +0400

    EX18_1 complete

--- 5. Files/EX18.py
+++ 5. Files/EX18.py
@@ -9,4 +9,20 @@ def get_final_line(filename):
         return final_line
 
 
-print(get_final_line('stix.txt'))
+# print(get_final_line('stix.txt'))
+
+
+def get_sum_of_numbers_in_file(filename):
+    with open(os.path.join(os.getcwd(), filename), encoding='utf-8') as f:
+        sum_num_file = 0
+        for line in f:
+            splitting_line = line.split()
+            for string in splitting_line:
+                try:
+                    sum_num_file += int(string.strip(',.?;:'))
+                except ValueError:
+                    continue
+        return sum_num_file
+
+
+print(get_sum_of_numbers_in_file('stix.txt'))
