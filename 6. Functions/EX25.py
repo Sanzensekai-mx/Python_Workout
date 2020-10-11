757688905a5256b1000bcbb6929a555ae2591ff8
Sanzensekai-mx <Ivanymn@yandex.ru>
2020-10-11 23:11:11 +0400

    EX25_1 complete!

--- 6. Functions/EX25.py
+++ 6. Functions/EX25.py
@@ -3,6 +3,20 @@ def my_xml(tag_name, content='', **kwargs):
     return f'<{tag_name}{attr}>{content}</{tag_name}>'
 
 
-print(my_xml('foo'))
-print(my_xml('foo', 'bar'))
-print(my_xml('foo', 'bar', a=1, b=2, c=3))
+# print(my_xml('foo'))
+# print(my_xml('foo', 'bar'))
+# print(my_xml('foo', 'bar', a=1, b=2, c=3))
+
+
+#  EX25_1
+
+
+def copy_file(input_file_name, *args):
+    for file in args:
+        with open(input_file_name, 'r', encoding='utf-8') as in_file:
+            with open(file, 'w', encoding='utf-8') as to_file:
+                for line in in_file:
+                    to_file.write(line)
+
+
+copy_file('stix6.txt', 'stix6_1.txt', 'stix6_2.txt')
