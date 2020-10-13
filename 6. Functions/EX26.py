f64fe9b8768315652b3904c3f30bf69f6dedcb95
Sanzensekai-mx <Ivanymn@yandex.ru>
2020-10-13 21:13:15 +0400

    EX25 main complete

---
+++ 6. Functions/EX26.py
@@ -0,0 +1,18 @@
+import operator
+
+
+def calc(string_to_solve):
+    operations = {'+': operator.add,
+                  '-': operator.sub,
+                  '*': operator.mul,
+                  '/': operator.truediv,
+                  '**': operator.pow,
+                  '%': operator.mod}
+    op, first, second = string_to_solve.split()
+    first, second = int(first), int(second)
+    return operations[op](first, second)
+
+
+print(calc('+ 1 2'))
+print(calc('- 1 2'))
+print(calc('* 2 2'))
