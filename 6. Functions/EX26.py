02958844469868f0aa627c13c9900906bc574575
Sanzensekai-mx <Ivanymn@yandex.ru>
2020-10-13 21:25:11 +0400

    EX26_1 complete

--- 6. Functions/EX26.py
+++ 6. Functions/EX26.py
@@ -19,3 +19,26 @@ def calc(string_to_solve):
 
 
 #  EX26_1
+
+
+def more_num_calc(string_to_solve):
+    operations = {'+': operator.add,
+                  '-': operator.sub,
+                  '*': operator.mul,
+                  '/': operator.truediv,
+                  '**': operator.pow,
+                  '%': operator.mod}
+    string_to_solve_to_list = string_to_solve.split()
+    op, args = string_to_solve_to_list[0], string_to_solve_to_list[1:]
+    args = [int(i) for i in args]
+    result = 0
+    for num in args:
+        if args.index(num) == 0:
+            result = num
+            continue
+        result = operations[op](result, num)
+    return result
+
+
+print(more_num_calc('+ 3 5 7'))
+print(more_num_calc('/ 100 5 5'))
