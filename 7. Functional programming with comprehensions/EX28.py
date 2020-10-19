844325380056247fea9b203309dbbfe512baa35a
Sanzensekai-mx <Ivanymn@yandex.ru>
2020-10-19 23:26:30 +0400

    EX28_1 complete

--- 7. Functional programming with comprehensions/EX28.py
+++ 7. Functional programming with comprehensions/EX28.py
@@ -2,4 +2,14 @@ def join_numbers(range_of_nums):
     return ', '.join([str(num) for num in range_of_nums])
 
 
-print(join_numbers(range(15)))
+# print(join_numbers(range(15)))
+
+
+#  EX28_1
+
+
+def join_numbers_2(range_of_nums):
+    return ', '.join([str(num) for num in range_of_nums if num in range(11)])
+
+
+print(join_numbers_2(range(15)))
