c27213bdda2b5356e6c99ea9cd64a76add68eed4
Sanzensekai-mx <Ivanymn@yandex.ru>
2020-10-20 23:16:17 +0400

    EX28_3 complete and EX28 all complete

--- 7. Functional programming with comprehensions/EX28.py
+++ 7. Functional programming with comprehensions/EX28.py
@@ -21,4 +21,15 @@ def sum_hex_num(list_hex_str):
     return sum([int(hex_str, 16) for hex_str in list_hex_str])
 
 
-print(sum_hex_num(['0x3e8', '0x7', '0x3f', '0xff']))
+# print(sum_hex_num(['0x3e8', '0x7', '0x3f', '0xff']))
+
+
+#  EX28_3
+
+# ?????
+def reverse_comprehension(text_file):
+    with open(text_file, 'r') as f:
+        return [' '.join(line.split()[::-1]) for line in f for line in line.splitlines()]
+
+
+print(reverse_comprehension('text_file_for_ex28_3.txt'))
