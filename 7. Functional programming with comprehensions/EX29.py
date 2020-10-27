7e4568579b71dc2cfef9e0ce1024c0f241cb8d29
Sanzensekai-mx <Ivanymn@yandex.ru>
2020-10-27 23:11:46 +0400

    EX29_2 complete

--- 7. Functional programming with comprehensions/EX29.py
+++ 7. Functional programming with comprehensions/EX29.py
@@ -7,6 +7,20 @@ def sum_numbers(string):
 #  EX29_1
 
 
-with open('stix.txt', 'r', encoding='utf-8') as f:
-    for line in [i.strip('\n') for i in f if len(i) > 20]:
-        print(line) if [vowel for vowel in line if vowel in 'aeiouy'] else None
+# with open('stix.txt', 'r', encoding='utf-8') as f:
+    # for line in [i.strip('\n') for i in f if len(i) > 20]:
+        # print(line) if [vowel for vowel in line if vowel in 'aeiouy'] else None
+
+
+#  EX29_2
+
+
+def covert_phone_num(list_of_num):
+    result_list = []
+    for i in [num.split('-') for num in list_of_num]:
+        result_list.append(f'{str(int(i[0]) + 1)}-{i[1]}-{i[2]}') if i[1][0] in '012345' \
+            else result_list.append(f'{i[0]}-{i[1]}-{i[2]}')
+    return result_list
+
+
+print(covert_phone_num(['123-456-7890', '123-333-4444', '123-777-8888']))
