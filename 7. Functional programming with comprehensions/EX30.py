8dd27532d49aee1123eeb16c61f63e9437ccc4ab
Sanzensekai-mx <Ivanymn@yandex.ru>
2020-10-28 23:09:35 +0400

    EX30 main and EX30_1 complete

--- 7. Functional programming with comprehensions/EX30.py
+++ 7. Functional programming with comprehensions/EX30.py
@@ -4,4 +4,16 @@ def flatten(list_of_lists):
             for one_el in one_sublist]  # Iterates through each element of one_sublist
 
 
-print(flatten([[1, 2], [3, 4]]))
+# print(flatten([[1, 2], [3, 4]]))
+
+
+# EX31_1
+
+
+def flatten_odd_ints(list_of_lists):
+    return [one_el for one_sublist in list_of_lists
+            for one_el in one_sublist if type(one_el) == int and one_el % 2 != 0]
+
+
+print(flatten_odd_ints([[1, 2], [3, 4]]))
+print(flatten_odd_ints([[1, 2, 3.0], [13, 80.1], ['string', 5]]))
