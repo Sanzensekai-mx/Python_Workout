1d8f955cf131791b1d53ea189f7afc6aa1f288a3
Sanzensekai-mx <Ivanymn@yandex.ru>
2020-10-29 09:40:06 +0300

    EX30_3 and EX30 all complete!

--- 7. Functional programming with comprehensions/EX30.py
+++ 7. Functional programming with comprehensions/EX30.py
@@ -33,12 +33,8 @@ child_grandchildren = {'A': ['B', 'C', 'D'], 'E': ['F', 'G']}
 
 child_grandchildren = {'Igor': [{'name': 'Ivan', 'age': '20'}, {'name': 'El', 'age': '21'}],
                        'Liza': [{'name': 'Mica', 'age': '18'}, {'name': 'Kl', 'age': '12'}]}
-# print(child_grandchildren.values())
-
-# print([one_grandchild['name'] for one_child in child_grandchildren.values() for one_grandchild in sorted(one_child, key=operator.itemgetter('age', 'name'))])
-# print(map(lambda ll: [el for lst in ll for el in lst], [one_child for one_child in child_grandchildren.values()])))
 lst_merge = []
 [lst_merge.append(dict_grand_child) for one_child in [one_child for one_child in child_grandchildren.values()]
  for dict_grand_child in one_child]
-# print(lst_merge)
-print(sorted([one_grand_child['name'] for one_grand_child in lst_merge], key=max))
+print(sorted(lst_merge, key=lambda x: x['age']))
+print([one_grand_name['name'] for one_grand_name in sorted(lst_merge, key=lambda x: x['age'])])
