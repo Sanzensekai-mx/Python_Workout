e13d5ce53ebcb85d48b05cc5aba531b6691681e8
Sanzensekai-mx <ivanymn@yandex.ru>
2020-11-12 23:55:39 +0400

    EX32_1 complete

--- 7. Functional programming with comprehensions/EX32.py
+++ 7. Functional programming with comprehensions/EX32.py
@@ -6,4 +6,14 @@ def flip_dict(input_dict):
     return {value: key for key, value in d.items()}
 
 
-print(flip_dict(d))
+# print(flip_dict(d))
+
+#  EX32_1
+example_str = 'this is an easy test'
+
+
+def count_vowels_in_words_in_str(string):
+    return {word: len([i for i in word if i in 'aeoiuy']) for word in string.split()}
+
+
+print(count_vowels_in_words_in_str(example_str))
