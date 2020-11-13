621bcdacb2ed790f15dcc86a57faa39f3186bf05
Sanzensekai-mx <ivanymn@yandex.ru>
2020-11-13 23:07:38 +0400

    EX33_2 complete

--- 7. Functional programming with comprehensions/EX33.py
+++ 7. Functional programming with comprehensions/EX33.py
@@ -3,4 +3,18 @@ def transform_values(func, input_dict):
 
 
 d = {'a': 1, 'b': 2, 'c': 3}
-print(transform_values(lambda x: x * x, d))
+
+
+# print(transform_values(lambda x: x * x, d))
+
+
+# EX33_1
+# Дебильное упражнение, непонятное нихуя
+# EX33_2
+
+def passwd_to_dict(passwd_file):
+    return {user_name: user_id for string in open(passwd_file)
+            for user_name, user_id in {string.split(':')[0]: string.split(':')[2]}.items()}
+
+
+print(passwd_to_dict('passwd'))
