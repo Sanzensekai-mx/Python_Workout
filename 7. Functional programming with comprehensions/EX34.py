51d15e4bc2408e068991488f051373eca28011d8
Sanzensekai-mx <ivanymn@yandex.ru>
2020-11-16 22:53:50 +0400

    EX34_3 complete

--- 7. Functional programming with comprehensions/EX34.py
+++ 7. Functional programming with comprehensions/EX34.py
@@ -23,5 +23,16 @@ def get_length(file_name):
     return {len(word.strip()) for word in open(file_name)}
 
 
-print(get_length('words.txt'))
-print(get_length('stix.txt'))
+# print(get_length('words.txt'))
+# print(get_length('stix.txt'))
+
+
+# EX34_3
+family = ['Ivan', 'Keks', 'Svetlana', 'Luda', 'Olga']
+
+
+def vowels_my_family(list_of_string):
+    return {vowel for name in list_of_string for vowel in set(name.lower())}
+
+
+print(vowels_my_family(family))
