624ec46ea68facadac4ce7eff1f48e98a8859518
Sanzensekai-mx <ivanymn@yandex.ru>
2020-11-16 22:46:01 +0400

    EX34_1 complete

--- 7. Functional programming with comprehensions/EX34.py
+++ 7. Functional programming with comprehensions/EX34.py
@@ -3,4 +3,14 @@ def get_cv(file_name):
     return {word.strip() for word in open(file_name) if vowels < set(word.lower())}
 
 
-print(get_cv('words.txt'))
+# print(get_cv('words.txt'))
+
+
+# EX34_1
+
+
+def get_user_shell(passwd_file):
+    return {string.strip().split(':')[-1] for string in open(passwd_file)}
+
+
+print(get_user_shell('passwd'))
