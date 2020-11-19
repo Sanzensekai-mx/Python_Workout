117f000bbeb3ef806c82a1afc86609053b0c3383
Sanzensekai-mx <ivanymn@yandex.ru>
2020-11-19 22:56:37 +0400

    EX35a_1 complete

--- 7. Functional programming with comprehensions/EX35a.py
+++ 7. Functional programming with comprehensions/EX35a.py
@@ -5,4 +5,15 @@ def gematria_dict():
     return {char: index for index, char in enumerate(string.ascii_lowercase, 1)}
 
 
-print(gematria_dict())
+# print(gematria_dict())
+
+
+# EX35a_1
+
+
+def config_to_dict(filename):
+    return {key.strip(): value.strip() for string in open(filename)
+            for key, value in {string.split('=')[0]: string.split('=')[1]}.items()}
+
+
+print(config_to_dict('config.txt'))
