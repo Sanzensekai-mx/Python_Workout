853cbb40435ff9b84f59b36ffd5b98d6e750da95
Sanzensekai-mx <ivanymn@yandex.ru>
2020-11-20 00:25:18 +0400

    EX35a_3 complete

--- 7. Functional programming with comprehensions/EX35a.py
+++ 7. Functional programming with comprehensions/EX35a.py
@@ -1,4 +1,5 @@
 import string
+import json
 
 
 def gematria_dict():
@@ -26,4 +27,23 @@ def config_to_dict_expand(filename):
             for key, value in {string_i.split('=')[0]: string_i.split('=')[1]}.items() if value.strip().isdigit()}
 
 
-print(config_to_dict_expand('config.txt'))
+# print(config_to_dict_expand('config.txt'))
+
+
+# EX35a_2
+
+
+def transform_json_to_dict(json_file):
+    # Название, население в дикт
+    return {name_city: population_city for dictionary in json.load(open(json_file))
+            for name_city, population_city in {dictionary['city']: dictionary['population']}.items()}
+
+
+def transform_json_to_another_dict(json_file):
+    return {(city_state, name_city): population_city for dictionary in json.load(open(json_file))
+            for name_city, population_city in {dictionary['city']: dictionary['population']}.items()
+            for city_state in {None: dictionary['state']}.values()}
+
+
+print(transform_json_to_dict('cities.json'))
+print(transform_json_to_another_dict('cities.json'))
