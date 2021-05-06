20233c0591d325afe74432618773764e87a87c09
Sanzensekai-mx <ivanymn@yandex.ru>
2021-05-06 11:49:25 +0400

    Full complete Ex 37

---
+++ 8. Modules and packages/EX37_Menu.py
@@ -0,0 +1,21 @@
+from menu import menu
+from staff import *
+
+
+def func_a():
+    return 'A'
+
+
+def func_b():
+    return 'B'
+
+
+# return_value = menu(a=func_a, b=func_b)
+# print(f'Result is {return_value}')
+
+# Ex 37c
+bar()
+foo()
+print(a)
+print(b)
+print(c)
---
+++ 8. Modules and packages/staff.py
@@ -0,0 +1,14 @@
+__all__ = ['a', 'c', 'bar']
+
+a = 'A'
+b = 'B'
+c = 'C'
+
+
+def foo():
+    print('Foo')
+
+
+def bar():
+    print('Bar')
+
