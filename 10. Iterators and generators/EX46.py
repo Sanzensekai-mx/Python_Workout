e2f093f74e8b8fb088991c24a4e3cbd30b8a20ab
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2021-07-07 19:13:16 +0400

    EX46 complete

--- 10. Iterators and generators/EX46.py
+++ 10. Iterators and generators/EX46.py
@@ -21,7 +21,7 @@ class LoudIterator:
 
 
 # for one_item in LoudIterator('abc'):
-    # print(one_item)
+# print(one_item)
 
 
 def foo():
@@ -34,20 +34,45 @@ def foo():
 
 
 class MyEnumerate:
-    def __init__(self, data):
+    def __init__(self, data, start_index=0):
         self.data = data
+        self.start_index = start_index
         self.index = 0
 
     def __iter__(self):
-        return MyEnumerate(self.data)
+        return MyEnumerate(self.data, self.start_index)
+        # return self
 
     def __next__(self):
         if self.index >= len(self.data):
             raise StopIteration
-        value = (self.index, self.data[self.index])
+        value = (self.index + self.start_index, self.data[self.index])
+        # print(f'**** {value}')
         self.index += 1
         return value
 
 
-for idx, v in MyEnumerate('abcdefg'):
+for idx, v in MyEnumerate('abcdefg', 1):
     print(f'{idx} - {v}')
+
+
+# x = MyEnumerate('heghlslsdgjk')
+#
+# print('***A***')
+# for i, v in x:
+#     print(f'{i} - {v}')
+#
+# print('***B***')
+# for i, v in x:
+#     print(f'{i} - {v}')
+
+def my_enumerate(data, start_index=0):
+    index = 0
+    for d in data:
+        value = (index + start_index, d)
+        index += 1
+        yield value
+
+
+for i, v in my_enumerate('abcdefg', 1):
+    print(f'{i} - {v}')
