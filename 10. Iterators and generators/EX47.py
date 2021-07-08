7444530b909ea84ef113cabcef5c019c572c3fb5
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2021-07-08 14:09:17 +0400

    EX47 complete

--- 10. Iterators and generators/EX47.py
+++ 10. Iterators and generators/EX47.py
@@ -21,8 +21,8 @@ class Circle:
         return CircleIterator(self.num, self.sequence)
 
 
-c = Circle('abc', 5)
-print(list(c))
+# c = Circle('abc', 5)
+# print(list(c))
 
 
 class Circle:
@@ -46,6 +46,49 @@ class Circle(CircleIterator):
         return Circle(self.sequence, self.num)
 
 
-print('*************')
-c = Circle('abc', 5)
-print(list(c))
+# print('*************')
+# c = Circle('abc', 5)
+# print(list(c))
+
+
+# 2
+
+def circle(sequence, num):
+    return (sequence[x % len(sequence)] for x in range(num))
+
+
+# print('*************')
+# print(list(circle('abc', 5)))
+
+
+# 3
+
+class MyRangeIterator:
+    def __init__(self, start: int, stop=None, step=1):
+        if stop is None:
+            self.stop = start
+            self.start = 0
+        else:
+            self.start = start
+            self.stop = stop
+        self.step = step
+
+    def __next__(self):
+        while self.start < self.stop:
+            value = self.start
+            self.start += self.step
+            return value
+        else:
+            raise StopIteration
+
+
+class MyRange(MyRangeIterator):
+    def __init__(self, start: int, stop=None, step=1):
+        super().__init__(start, stop, step)
+
+    def __iter__(self):
+        return MyRangeIterator(self.start, self.stop, self.step)
+
+
+for i in MyRange(5, 10, 2):
+    print(i)
