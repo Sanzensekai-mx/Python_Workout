5f12b66d162033767b68d4a2c462805168b8637f
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2021-07-08 14:29:53 +0400

    EX48_1

--- 10. Iterators and generators/EX48.py
+++ 10. Iterators and generators/EX48.py
@@ -2,11 +2,11 @@ import os
 
 
 def read_all_lines(path_to_dir):
-    for file in os.listdir(path_to_dir):
+    for num_file, file in enumerate(os.listdir(path_to_dir), 1):
         with open(os.path.join(path_to_dir, file), 'r', encoding='utf-8') as r:
             try:
-                for line in r:
-                    yield line
+                for num_line, line in enumerate(r, 1):
+                    yield file, num_file, num_line, line
             except OSError:
                 pass
 
